5cbcef327e74b1a9a9b0425674403332e6e8e4c0
MARVEKS <112755028+MARVEKS@users.noreply.github.com>
2025-05-25 10:06:08 +0300

    Update 3.py

--- laba3/3.py
+++ laba3/3.py
@@ -1,24 +1,16 @@
 import copy
 
-# Клас Animal з властивостями name та species
-# Важливо: простий клас для демонстрації шаблону проектування Prototype
 class Animal:
     def __init__(self, name, species):
         self.name = name
         self.species = species
 
-    # Метод speak виводить інформацію про тварину
     def speak(self):
         print(f"Моє ім'я {self.name}, і я {self.species}.")
 
-    # Метод clone реалізує глибоке копіювання об'єкта
-    # Важливо: це і є реалізація шаблону Prototype
     def clone(self):
         return copy.deepcopy(self)
 
-# Основна функція
-# Важливо: демонструє роботу патерну Prototype на практиці
-# Клонований об'єкт змінюється незалежно від оригіналу
 
 def main():
     original_dog = Animal("Рекс", "собака")
@@ -26,10 +18,9 @@ def main():
 
     print("\nКлонування собаки...\n")
 
-    cloned_dog = original_dog.clone()  # створення копії (прототипу)
-    cloned_dog.name = "Фінік"  # зміна ім'я клонованої тварини
+    cloned_dog = original_dog.clone()
+    cloned_dog.name = "Фінік"
 
-    # Оригінал не змінюється — важливо для шаблону Prototype
     original_dog.speak()
     cloned_dog.speak()
 
